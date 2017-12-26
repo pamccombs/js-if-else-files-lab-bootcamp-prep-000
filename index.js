4363d8b7a156fc3ce00b7764aec8a9d26e4cc590
Patrick McCombs <pa.mccombs1@gmail.com>
2017-12-26 15:07:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var name = "Susan"
 var height = "74"
 if (name === "Susan") {
   console.log("The name is correct")
+} else if (height === "74") {
+  console.log("The height is correct")
 } else {
   console.log("Expected the name to be Susan")
 }
